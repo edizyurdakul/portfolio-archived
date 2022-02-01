e34529a2cbf06424863b44d4eb5011bc2c219924
Ediz <edizyurdakul@gmail.com>
2022-02-01 15:29:16 +0200

    Update prefer theme for system color mode preference

--- lib/theme.ts
+++ lib/theme.ts
@@ -3,7 +3,7 @@ import { mode } from "@chakra-ui/theme-tools";
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
-  useSystemColorMode: false,
+  useSystemColorMode: true,
 };
 
 const theme = extendTheme({
